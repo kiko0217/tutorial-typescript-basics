b6010cf2b0bc24cf736bbaaaaa315323ba001541
kiko0217 <anziskita@gmail.com>
2022-07-18 06:50:01 +0700

    finish basic tyscript

--- index.ts
+++ index.ts
@@ -1,3 +1,44 @@
+import * as _ from 'lodash'
+
 async function hello() {
     return 'world'
 }
+
+// const url = new URL('...')
+
+let lucky;
+lucky = '23'
+
+type TestType = string;
+
+let testType: TestType = "test";
+
+
+type Style = 'bold' | 'italid' | 23;
+
+let font:Style = 23;
+
+interface Person {
+    first: string
+    last: string
+    [key: string]: any
+}
+
+
+const person:Person = {
+    first: 'jeff',
+    last: 'delaney',
+    fast: true
+}
+
+function pow(x:number, y:number): string {
+    return Math.pow(x, y).toString()
+}
+
+type MyList = [number?, string?, boolean?]
+
+let arr:MyList;
+arr.push(1)
+arr.push("test")
+arr.push(false)
+
